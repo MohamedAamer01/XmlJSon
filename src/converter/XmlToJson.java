b24622b8a4208d01a12e448325decf142a580bbd
Mohamed <mohamedaamer0622@gmail.com>
2026-01-05 00:55:51 +0100

    XmlToJson.java modifie

--- src/converter/XmlToJson.java
+++ src/converter/XmlToJson.java
@@ -5,25 +5,19 @@ import org.json.XML;
 
 public class XmlToJson {
 
-    /**
-     * Convertit un XML (texte) en JSON
-     * @param xml contenu XML saisi par l'utilisateur
-     * @param removeRoot supprimer la racine <root> si true
-     * @return JSON formaté
-     */
     public static String convertFromString(String xml, boolean removeRoot) throws Exception {
 
         if (xml == null || xml.trim().isEmpty()) {
             throw new Exception("Le XML est vide");
         }
-
+        
         JSONObject json = XML.toJSONObject(xml);
 
         if (removeRoot && json.has("root")) {
             json = json.getJSONObject("root");
         }
 
-        return json.toString(4);
+        return json.toString(4);  // pour ajouter les espaces pour l'optimisation de Json file 
     }
 }
  
